8d398088ab387a3db652fdf1a128f0e0b3efedef
Gothic <kalcifer_88@mail.ru>
2024-05-25 22:25:27 +0300

    Add files via upload

---
+++ StringLesson.py
@@ -0,0 +1,8 @@
+#Строки и индексация строк
+example = "Follow the white rabbit"
+print(example[0])
+print(example[-1])
+print(len(example))
+print(example[12:])
+print(example[::-1])
+print(example[1::2])
